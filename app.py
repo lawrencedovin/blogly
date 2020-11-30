32bd3b31452aa626535907b207b26fc5f572ce76
Lawrence Dovin <lawrencedovin@gmail.com>
2020-11-29 19:58:28 -0500

    Edit user function works! hooray :)

--- app.py
+++ app.py
@@ -20,6 +20,48 @@ def list_users():
 
     return render_template('list.html', users=users)
 
+@app.route('/users/new')
+def show_create_user_form():
+    """Get view for create user form"""
+    return render_template('create-form.html')
+
+@app.route('/users/new', methods=['POST'])
+def create_user():
+    first_name = request.form['firstName']
+    last_name = request.form['lastName']
+    image_url = request.form['imageUrl']
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(f'/users/{new_user.id}')
+
+@app.route('/users/<int:user_id>')
+def get_user_details(user_id):
+    user = User.query.get_or_404(user_id)
+    full_name = user.get_full_name()
+    return render_template('details.html', user=user, full_name=full_name)
+
+@app.route('/users/<int:user_id>/edit')
+def show_edit_user_form(user_id):
+    user = User.query.get_or_404(user_id)
+    full_name = user.get_full_name()
+    return render_template('edit-form.html', user=user, full_name=full_name)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['firstName']
+    user.last_name = request.form['lastName']
+    user.image_url = request.form['imageUrl']
+
+    db.session.commit()
+    return redirect('/')
+
+
+
 # @app.route('/list')
 # def list_pets():
 #     """List pets and show add form."""
